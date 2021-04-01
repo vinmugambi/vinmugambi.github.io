875db3eae42873b042547574df6932870262b84e
vinmugambi <vinmugambi@users.noreply.github.com>
2021-04-01 17:26:29 +0000

    deploy: 87443008cd58891b219fdc0a8f5029bb11fe6383

---
+++ _nuxt/static/1617297985/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1617297985",layout:"default",error:b,serverRendered:true,routePath:a,config:{app:{basePath:a,assetsPath:"\u002F_nuxt\u002F",cdnURL:b},content:{dbHash:"f5822939"},http:{browserBaseURL:a}}}}("\u002F",null));
