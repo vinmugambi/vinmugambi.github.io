b57021f09a6591359302c46a38a307aab9efaea5
vinmugambi <vinmugambi@users.noreply.github.com>
2022-11-14 19:08:11 +0000

    deploy: 53c0e72a2850b56f9e59d8fe3a29421dad71c4d0

---
+++ _nuxt/static/1668452890/blog/pdf_documents/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1668452890",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fpdf_documents",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"fe39dc4e"},http:{browserBaseURL:b}}}}(null,"\u002F"));
