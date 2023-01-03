b19d99b7170a570de97d0a0f5fe70718f69d09e3
vinmugambi <vinmugambi@users.noreply.github.com>
2023-01-03 10:37:37 +0000

    deploy: 7a1d2be19aff499c18ac1c1c794a12bd65528c66

--- _nuxt/static/1672393884/blog/debug-nuxt-on-vs-code/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1672393884",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fdebug-nuxt-on-vs-code",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"dac07bef"},http:{browserBaseURL:b}}}}(null,"\u002F"));
